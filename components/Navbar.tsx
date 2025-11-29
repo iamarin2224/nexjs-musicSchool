d629c3a23085ddaa48ae77632716ccd4fa675134
Arin Das <iamarindas@gmail.com>
2025-11-29 19:40:49 +0530

    Complete Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+
+        <Link href={"/"}>
+        <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+        </Link>
+
+        
+        <MenuItem setActive={setActive} active={active} item="Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/hobby">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/individual">Advanced Composition</HoveredLink>
+            <HoveredLink href="/team">Song Writing</HoveredLink>
+            <HoveredLink href="/enterprise">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+        
+
+        <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem>
+
+      </Menu>
+    </div>
+  )
+}
